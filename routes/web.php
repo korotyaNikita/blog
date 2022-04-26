9ca524b203c2de4972c63e9e124b8dfe8fea2e6f
korotyaNikita <korotinnikit@gmail.com>
2022-04-26 23:21:29 +0300

    add ShowController

--- routes/web.php
+++ routes/web.php
@@ -27,8 +27,8 @@ Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'],
         Route::get('/', IndexController::class)->name('admin.categories.index');
         Route::get('/create', CreateController::class)->name('admin.categories.create');
         Route::post('/', StoreController::class)->name('admin.categories.store');
-        /*Route::get('/{category}', ShowController::class)->name('admin.categories.show');
-        Route::get('/{category}/edit', EditController::class)->name('admin.categories.edit');
+        Route::get('/{category}', ShowController::class)->name('admin.categories.show');
+        /*Route::get('/{category}/edit', EditController::class)->name('admin.categories.edit');
         Route::patch('/{category}', UpdateController::class)->name('admin.categories.update');
         Route::delete('/{category}', DestroyController::class)->name('admin.categories.destroy');*/
     });
