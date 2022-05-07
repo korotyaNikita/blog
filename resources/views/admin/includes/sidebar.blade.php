fb8dc29bd46ad5721a616796bea0e703d2deb487
korotyaNikita <korotinnikit@gmail.com>
2022-05-07 20:20:41 +0300

    add tags crud

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -11,6 +11,14 @@
                     </p>
                 </a>
             </li>
+            <li class="nav-item">
+                <a href="{{ route('admin.tags.index') }}" class="nav-link">
+                    <i class="nav-icon far fa-image"></i>
+                    <p>
+                        Теги
+                    </p>
+                </a>
+            </li>
         </ul>
     </div>
     <!-- /.sidebar -->
