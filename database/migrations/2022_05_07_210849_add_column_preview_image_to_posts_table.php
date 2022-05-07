05e377edb68c4a1e73b96c21f6ae1ca1da2b8e48
korotyaNikita <korotinnikit@gmail.com>
2022-05-08 00:45:28 +0300

    add image create

--- database/migrations/2022_05_07_210849_add_column_preview_image_to_posts_table.php
+++ database/migrations/2022_05_07_210849_add_column_preview_image_to_posts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->string('image')->after('content')->nullable();
+            $table->string('preview_image')->after('content')->nullable();
         });
     }
 
@@ -26,7 +26,7 @@ return new class extends Migration
     public function down()
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->dropColumn('image');
+            $table->dropColumn('preview_image');
         });
     }
 };
